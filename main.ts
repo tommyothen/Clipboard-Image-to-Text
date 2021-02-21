c5562d3639f3e7b0612456186e753a530a18f17c
DaSushiAsian <42751764+DaSushiAsian@users.noreply.github.com>
2021-02-21 16:26:05 +0000

    State app is ready

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { app, Tray, Menu, BrowserWindow } from "electron";
 import { createWorker } from "tesseract.js";
 import * as path from "path";
 
+const worker = createWorker();
+
 app.on("ready", async () => {
   const tray = new Tray(path.join(__dirname, "../icon.png"));
   tray.setContextMenu(Menu.buildFromTemplate([
@@ -14,4 +16,5 @@ app.on("ready", async () => {
   ]));
 
   new BrowserWindow({ show: false });
+  console.log("App is ready!");
 });
